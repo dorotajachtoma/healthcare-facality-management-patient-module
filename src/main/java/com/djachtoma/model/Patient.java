07554e5f63f2f91094fa6fd04dec104e785b3885
dorotajachtoma <jachtoma.do@gmail.com>
2023-08-25 19:47:32 +0200

    setup basic keyspace for Patient entity

--- src/main/java/com/djachtoma/model/Patient.java
+++ src/main/java/com/djachtoma/model/Patient.java
@@ -1,13 +1,23 @@
 package com.djachtoma.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@RedisHash("Patient")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("patient")
 public class Patient implements Serializable {
 
+    @Id
     private String id;
     private String name;
     private String surname;
