74e9343ca3056e2c37306a30ee93ec433e9ccafd
dorotajachtoma <jachtoma.do@gmail.com>
2023-08-28 17:23:04 +0200

    controller setup & service for Patient entity - part III

--- src/main/java/com/djachtoma/model/patient/dto/PatientMapper.java
+++ src/main/java/com/djachtoma/model/patient/dto/PatientMapper.java
@@ -1,10 +1,13 @@
 package com.djachtoma.model.patient.dto;
 
 import com.djachtoma.model.Gender;
+import com.djachtoma.model.address.Address;
 import com.djachtoma.model.id.IDCard;
 import com.djachtoma.model.patient.Patient;
 import com.djachtoma.model.phone.PhoneNumber;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PatientMapper {
 
     public static PatientDTO toDTO(Patient patient) {
@@ -15,6 +18,9 @@ public class PatientMapper {
                 .dateOfBirth(patient.getDateOfBirth())
                 .phoneNumber(patient.getPhoneNumber().getNumber())
                 .idCardSeriesNumber(patient.getIdCard().getSeriesNumber())
+                .city(patient.getAddress().getCity())
+                .zipCode(patient.getAddress().getZipCode())
+                .address(patient.getAddress().getAddress())
                 .build();
     }
 
@@ -29,6 +35,11 @@ public class PatientMapper {
                         .build())
                 .phoneNumber(PhoneNumber.builder()
                         .number(patientDTO.getPhoneNumber())
+                        .build())
+                .address(Address.builder()
+                        .city(patientDTO.getCity())
+                        .zipCode(patientDTO.getZipCode())
+                        .address(patientDTO.getAddress())
                         .build())
                 .build();
     }
