a9f56a76ed3fc4c8de93958269d43179457ebd8c
dorotajachtoma <jachtoma.do@gmail.com>
2023-08-29 18:09:44 +0200

    swagger setup for webflux

--- src/main/java/com/djachtoma/configuration/database/ConnectionProperties.java
+++ src/main/java/com/djachtoma/configuration/database/ConnectionProperties.java
@@ -1,4 +1,4 @@
-package com.djachtoma.configuration;
+package com.djachtoma.configuration.database;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
