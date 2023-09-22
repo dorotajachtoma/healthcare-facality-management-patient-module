acabb28ee7d8fb0a9a0596544ec8997f163d6a8b
dorotajachtoma <jachtoma.do@gmail.com>
2023-09-22 17:26:52 +0200

    bugfix for unit and integration tests

--- src/main/java/com/djachtoma/controller/PatientController.java
+++ src/main/java/com/djachtoma/controller/PatientController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.security.Principal;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -26,33 +24,32 @@ public class PatientController {
     private final PatientService patientService;
 
     @GetMapping
-    public Flux<PatientDTO> getPatients(Principal principal) {
-        log.info("%s is invoked by: %s", this.getClass().getName() + ".getPatients", principal.getName());
+    public Flux<PatientDTO> getPatients() {
+        log.info("%s is invoked.", this.getClass().getName() + ".getPatients");
         return patientService.getPatients();
     }
 
     @GetMapping("/{id}")
-    public Mono<PatientDTO> getPatient(@PathVariable String id, Principal principal) {
-        log.info("%s is invoked by: %s", this.getClass().getName() + ".getPatient", principal.getName());
+    public Mono<PatientDTO> getPatient(@PathVariable String id) {
+        log.info("%s is invoked.", this.getClass().getName() + ".getPatient");
         return patientService.getPatient(id);
     }
 
     @PostMapping
-    public Mono<PatientDTO> createPatient(@RequestBody PatientDTO patientDTO, Principal principal) {
-        log.info("%s is invoked by: %s", this.getClass().getName() + ".createPatient", principal.getName());
+    public Mono<PatientDTO> createPatient(@RequestBody PatientDTO patientDTO) {
+        log.info("%s is invoked.", this.getClass().getName() + ".createPatient");
         return patientService.createPatient(patientDTO);
     }
 
     @PatchMapping("/{id}")
-    public Mono<PatientDTO> updatePatient(@PathVariable String id, @RequestBody PatientDTO patientDTO,
-                                          Principal principal) {
-        log.info("%s is invoked by: %s", this.getClass().getName() + ".updatePatient", principal.getName());
+    public Mono<PatientDTO> updatePatient(@PathVariable String id, @RequestBody PatientDTO patientDTO) {
+        log.info("%s is invoked.", this.getClass().getName() + ".updatePatient");
         return patientService.updatePatient(id, patientDTO);
     }
 
     @DeleteMapping("/{id}")
-    public void deletePatient(@PathVariable String id, Principal principal) {
-        log.info("%s is invoked by: %s", this.getClass().getName() + ".deletePatient", principal.getName());
+    public void deletePatient(@PathVariable String id) {
+        log.info("%s is invoked.", this.getClass().getName() + ".deletePatient");
         patientService.deletePatient(id);
     }
 }
